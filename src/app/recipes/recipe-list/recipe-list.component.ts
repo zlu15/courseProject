89e517aa740032bda617a3b0d32ff71f68b7d3ff
Lu, Charlie <CHARLIE_LU@homedepot.com>
2020-12-17 20:34:32 -0500

    first commit for syn in different devices

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://tastesbetterfromscratch.com/wp-content/uploads/2010/06/Hersheys-Perfectly-Chocolate-Chocolate-Cake-13.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://tastesbetterfromscratch.com/wp-content/uploads/2010/06/Hersheys-Perfectly-Chocolate-Chocolate-Cake-13.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
